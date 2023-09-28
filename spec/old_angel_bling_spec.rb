925e5a8858595ad84791becf97447ec24b69c5da
paulo-felipe <alparakleto@gmail.com>
2023-09-27 21:48:39 -0300

    fix initial specs.

--- spec/old_angel_bling_spec.rb
+++ spec/old_angel_bling_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe OldAngelBling do
   it "has a version number" do
     expect(OldAngelBling::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
